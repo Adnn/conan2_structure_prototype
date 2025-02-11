6f5aaff46f8da6c285f749fb16d551333577ff73
Adrien <Adnn@users.noreply.github.com>
2025-02-11 10:12:12 +0100

    Demonstrate cases where component isolation is not enforced.

--- Mid/test_package/src/example.cpp
+++ Mid/test_package/src/example.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <string>
 
+// This is intended to work, even though it should not be done
+// (dowstreams should not rely on the fact that mid_a happens to use up_a)
+#include "up_a.h"
+
+// TODO #SHOULD_FAIL On the other hand, this should not work
+// up_b is not in the dependency graph of this test package
+#include "up_b.h"
+
 int main() {
     mid();
 
